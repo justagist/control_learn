d28d4634e849e55e0f821d7fe70519e1b1262868
justagist <saifksidhik@gmail.com>
2018-05-04 18:54:14 +0100

    add pid demo for cartpole env

---
+++ ctrl_lib_simple/demos/demo_cartpole_pid.py
@@ -0,0 +1,30 @@
+from ctrl_lib_simple.envs.cartpole import CartPoleEnv
+from ctrl_lib_simple.controllers.pid import PID
+import argparse
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description='Demo for controlling an inverted pendulum on a 1D cart using PID controller')
+
+    parser.add_argument("--cart_mass", default = 5.0, help = "Mass of cart")
+    parser.add_argument("--pole_length", default = 1.0, help = "Length of pendulum")
+    parser.add_argument("--pendulum_mass", default = 1.0, help = "Mass of pendulum bob")
+    parser.add_argument("--cart_start_pos", default = 0.2, help = "Starting position of cart")
+    parser.add_argument("--pendulum_start_angle", default = 1.0, help = "Starting orientation of pendulum")
+    parser.add_argument("--g", default = 9.81, help = "Acceleration due to gravity")
+
+    parser.add_argument("--desired_cart_pos", default = 0.3, help = "Desired final position of cart")
+    parser.add_argument("--desired_pendulum_angle", default = 0., help = "Desired final orientation (in rad) of pendulum (vertically up is 0)")
+
+    parser.add_argument("--Kp", default = -150, type = float, help = "The proportional gain of the PID controller")
+    parser.add_argument("--Kd", default = -20, type = float, help = "The derivative gain of the PID controller")
+    parser.add_argument("--Ki", default = -20, type = float, help = "The integral gain of the PID controller")
+
+    parser.add_argument("-P","--plot", default = 0, help = "Whether or not to plot the graphs for forces, error etc.")
+
+
+    args = parser.parse_args()
+
+    env = CartPoleEnv(cart_mass = args.cart_mass, pole_length = args.pole_length, pendulum_mass = args.pendulum_mass, cart_start_pos = args.cart_start_pos, pendulum_start_angle = args.pendulum_start_angle, g = args.g)
+
+    env.control_cartpole(control_type = 'pid', desired_cart_pos = args.desired_cart_pos, desired_pendulum_angle = args.desired_pendulum_angle, Kp = args.Kp, Kd = args.Kd, Ki = args.Ki)
